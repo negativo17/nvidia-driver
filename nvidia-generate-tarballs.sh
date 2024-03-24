b57b846079289653a11d0c3c565113b7874879d6
Simone Caronni <negativo17@gmail.com>
2024-03-24 11:47:39 +0100

    Update to 550.67

--- nvidia-generate-tarballs.sh
+++ nvidia-generate-tarballs.sh
@@ -2,7 +2,7 @@
 set -e
 
 set_vars() {
-   export VERSION=${VERSION:-550.54.14}
+   export VERSION=${VERSION:-550.67}
    export DL_SITE=${DL_SITE:-http://download.nvidia.com/XFree86}
    export TEMP_UNPACK=${ARCH}
    export PLATFORM=Linux-${ARCH}
