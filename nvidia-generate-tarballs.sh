65478a48b728be6ab928d104f142882a6880d14d
Simone Caronni <negativo17@gmail.com>
2025-12-18 18:30:52 +0100

    Update to 590.48.01

--- nvidia-generate-tarballs.sh
+++ nvidia-generate-tarballs.sh
@@ -2,7 +2,7 @@
 set -e
 
 set_vars() {
-   export VERSION=${VERSION:-590.44.01}
+   export VERSION=${VERSION:-590.48.01}
    export TEMP_UNPACK=${ARCH}
    export PLATFORM=Linux-${ARCH}
    export RUN_FILE=NVIDIA-${PLATFORM}-${VERSION}.run
